06b6334609cf5048310e0087e1e8280e1ce6c7e8
Alexandr Kovalchuk <shelter.play.akk@mail.ru>
2025-03-05 12:47:21 +0700

    modified:   alembic.ini 	modified:   app/__pycache__/database.cpython-312.pyc 	modified:   app/database.py 	new file:   app/migrations/README 	renamed:    app/migration/__pycache__/env.cpython-312.pyc -> app/migrations/__pycache__/env.cpython-312.pyc 	renamed:    app/migration/env.py -> app/migrations/env.py 	new file:   app/migrations/script.py.mako 	renamed:    app/migration/versions/1de8d337550b_initial_version.py -> app/migrations/versions/1de8d337550b_initial_version.py 	renamed:    app/migration/versions/__pycache__/1de8d337550b_initial_version.cpython-312.pyc -> app/migrations/versions/__pycache__/1de8d337550b_initial_version.cpython-312.pyc 	new file:   app/migrations/versions/__pycache__/de9220d58143_initial_revision.cpython-312.pyc 	new file:   app/migrations/versions/de9220d58143_initial_revision.py 	modified:   app/models/__pycache__/user.cpython-312.pyc 	modified:   app/models/user.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,5 @@
-from sqlalchemy import ForeignKey, func
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, declared_attr
-from multiprocessing import get_context
-from datetime import datetime
-from typing import Annotated
 
 
 DB_HOST = 'localhost'
@@ -18,3 +14,17 @@ DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT
 engine = create_async_engine(DATABASE_URL)
 new_session = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
+class Base(DeclarativeBase, AsyncAttrs):
+    __abstract__ = True
+    
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, unique=True, nullable=False)
+    
+    
+# alembic revision --autogenerate -m "Initial revision"
+# 
+#
--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,39 @@
-from sqlalchemy import Column, Integer, String
+from datetime import datetime
+
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base
 
-class User(Base):
-    __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+class User(Base):
+    __tablename__ = 'User'
+    
+    username: Mapped[str] = mapped_column(unique=True, nullable=True)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)# Хранит хэшированный пароль
+
+    groups = relationship('QGroupModel', back_populates='user')
+    
+
+class QuestionModel(Base):
+    __tablename__ = 'Question'
+    
+    group_id: Mapped[int] = mapped_column(ForeignKey('Question_group.id'))
+    qstn: Mapped[str]
+    r_answr: Mapped[str]
+    wr_answr1: Mapped[str]
+    wr_answr2: Mapped[str]
+    wr_answr3: Mapped[str]
+    
+    group = relationship('QGroupModel', back_populates='questions')
+    
+    
+class QGroupModel(Base):
+    __tablename__ = 'Question_group'
+    
+    title: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey('User.id'))
+    created_at: Mapped[datetime] = mapped_column(server_default = func.now(), onupdate=datetime.now)
+    
+    questions = relationship('QuestionModel', back_populates='group')
+    user = relationship('UserModel', back_populates='groups')  
